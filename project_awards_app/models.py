2af72a5af2187a4f86f49707056708fadbc49837
Antavio <njuguna13@gmail.com>
2019-05-25 10:23:56 +0300

    Create a projects models to enable users to upload their projects.

--- project_awards_app/models.py
+++ project_awards_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    link = models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    image = models.ImageField(upload_to='project_pics',blank=True)
+    user_project_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+    
+
