4b3412be320a0cb38b1ef034f3cc54d5d06c7927
Antavio <njuguna13@gmail.com>
2019-05-25 12:17:18 +0300

    Create a new template that will display the upload form.

--- project_awards_app/urls.py
+++ project_awards_app/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name='home'),
+    url(r'^new/project$',views.new_project,name='new-project')
 ]
 
 if settings.DEBUG:
--- project_awards_app/views.py
+++ project_awards_app/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from .models import Project
+from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     all_projects = Project.fetch_all_images()
     return render(request,"Moringa_Project_Awards/index.html",{"all_images":all_projects})
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            user_image = form.save(commit=False)
+            user_image.user = current_user
+            user_image.save()
+        return redirect('home')
+    else:
+        form = ProjectForm()
+    return render(request,"Moringa_Project_Awards/new_project.html",{"form":form})
