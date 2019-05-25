f8afd17d509651c79020213b017490468dbea40a
Antavio <njuguna13@gmail.com>
2019-05-25 15:43:42 +0300

    Add a new view function to display edit profile form.

--- project_awards_app/views.py
+++ project_awards_app/views.py
@@ -23,7 +23,7 @@ def new_project(request):
         form = ProjectForm()
     return render(request,"Moringa_Project_Awards/new_project.html",{"form":form})
 
-
+@login_required(login_url='/accounts/login/')
 def new_profile(request):
     current_user = request.user
     if request.method == 'POST':
@@ -39,6 +39,19 @@ def new_profile(request):
     return render(request, 'profile/new_profile.html', {"form": form})
 
 @login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request,'profile/edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
 def profile(request):
     current_user = request.user
     projects = Project.objects.filter(user = current_user)
@@ -47,5 +60,5 @@ def profile(request):
         prof = Profile.objects.get(prof_user=current_user)
     except ObjectDoesNotExist:
         return redirect('new_profile')
-        
+
     return render(request,'profile/profile.html',{'profile':prof,'projects':projects,'current_user':current_user})
