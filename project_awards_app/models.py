91dff28e92b95a5db655ee1953a449d5baa58ea8
Antavio <njuguna13@gmail.com>
2019-05-26 20:54:14 +0300

    Create additional fields which will hold design,content,usability and total votes columns.

--- project_awards_app/models.py
+++ project_awards_app/models.py
@@ -10,6 +10,10 @@ class Project(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     image = models.ImageField(upload_to='project_pics',blank=True)
     user_project_id = models.IntegerField(default=0)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
