3307f7447d79c2a95f4b0148d481b3453ffe075f
Antavio <njuguna13@gmail.com>
2019-05-27 10:15:38 +0300

    Add an API url to enable viewing of the data using a url.

--- project_awards_app/urls.py
+++ project_awards_app/urls.py
@@ -11,7 +11,8 @@ urlpatterns=[
     url(r'^new_profile/$',views.new_profile,name = 'new_profile'),
     url(r'^edit/profile/$',views.profile_edit,name = 'edit_profile'),
     url(r'^search/',views.search_project, name='search_results'),
-    url(r'^project/review/(\d+)',views.project_review,name='project_review')
+    url(r'^project/review/(\d+)',views.project_review,name='project_review'),
+    url(r'^api/profile/$', views.ProfileList.as_view()),
 ]
 
 if settings.DEBUG:
